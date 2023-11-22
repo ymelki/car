e9291f7ac6ff57659d6abc5bb6783dac9c2c0856
ymelki <67371192+ymelki@users.noreply.github.com>
2023-11-22 12:47:22 +0100

    cart

--- confirm.php
+++ confirm.php
@@ -23,11 +23,23 @@ $cart=$_SESSION['cart'];
 var_dump($user);
 echo '<hr>';
 var_dump($cart);
+// insert 1 row in orders 
+$query = "INSERT INTO orders(idorder)
+    VALUES (NULL) ";
+$statement = $pdo->prepare($query);
+$statement->execute();
+
+// get last row in orders
+$query = "SELECT * FROM orders order by idorder desc limit 1";
+$statement = $pdo->query($query);
+$order = $statement->fetch(PDO::FETCH_ASSOC);
+
 
 foreach ($cart as $id_car=>$quantity){
-    $query = "INSERT INTO booking (user_id,car_id,quantity,start_date,date_end)
-    VALUES (:iduser, :idcar, :quantity, '2023-01-01', '2023-15-01') ";
+    $query = "INSERT INTO booking (id_order, user_id,car_id,quantity,start_date,date_end)
+    VALUES (:idorder,:iduser, :idcar, :quantity, '2023-01-01', '2023-12-01') ";
     $statement = $pdo->prepare($query);
+    $statement->bindValue(':idorder', $order['idorder'], \PDO::PARAM_INT);
     $statement->bindValue(':iduser', $user['id'], \PDO::PARAM_INT);
     $statement->bindValue(':idcar', $id_car, \PDO::PARAM_INT);
     $statement->bindValue(':quantity', $quantity, \PDO::PARAM_INT);
